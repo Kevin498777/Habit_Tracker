026ecd663b586c3bf32ad1af39c1272a80fc3c88
Kevin <kevinbrianip498@gmail.com>
2026-02-24 22:57:57 -0600

    cambios en app

--- app.py
+++ app.py
@@ -1004,6 +1004,4 @@ def add_security_headers(response):
 # RUN
 # -----------------------------
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    # En producción no usar debug=True
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
